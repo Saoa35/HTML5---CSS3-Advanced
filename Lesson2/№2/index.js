bcfcfab0ff3af7727bd4a7eb39e02cae9f653e44
Saoa35 <saoamah@gmail.com>
2022-02-23 12:17:19 +0100

    + hidemapbutton

--- Lesson2/№2/index.js
+++ Lesson2/№2/index.js
@@ -1,9 +1,14 @@
-// let showMapButton = document.querySelectorAll('.showMap');
-let showMapButton = document.querySelector('.showMap1');
+let showMapButton = document.querySelectorAll('.showMap');
+// let showMapButton = document.querySelector('.showMap1');
+let hidemapButton = document.querySelector('.hideMap1');
 let mapBlock = document.querySelector('.mapBlock');
 
 console.log(mapBlock);
 
 showMapButton.addEventListener('click', function() {
     mapBlock.setAttribute('class', 'mapBlockShow');
+});
+
+hidemapButton.addEventListener('click', function() {
+    mapBlock.setAttribute('class', 'mapBlock');
 });
