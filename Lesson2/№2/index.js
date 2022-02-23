325eb18ab333a412ff3716dcd7de7d81bd8141c2
Saoa35 <saoamah@gmail.com>
2022-02-23 12:49:07 +0100

    recoding all listeners

--- Lesson2/№2/index.js
+++ Lesson2/№2/index.js
@@ -1,14 +1,27 @@
 let showMapButton = document.querySelectorAll('.showMap');
+let hidemapButton = document.querySelectorAll('.hideMap');
+let mapBlock = document.querySelectorAll('.mapBlock');
+
+showMapButton.forEach(el => el.addEventListener('click', function() {
+    mapBlock.forEach(e => e.setAttribute('class', 'mapBlockShow')) 
+}));
+
+hidemapButton.forEach(el => el.addEventListener('click', function() {
+    mapBlock.forEach(e => e.setAttribute('class', 'mapBlock')) 
+}));
+
+
+
 // let showMapButton = document.querySelector('.showMap1');
-let hidemapButton = document.querySelector('.hideMap1');
-let mapBlock = document.querySelector('.mapBlock');
+// let hidemapButton = document.querySelector('.hideMap1');
+// let mapBlock = document.querySelector('.mapBlock');
 
-console.log(mapBlock);
+// console.log(showMapButton);
 
-showMapButton.addEventListener('click', function() {
-    mapBlock.setAttribute('class', 'mapBlockShow');
-});
+// showMapButton.addEventListener('click', function() {
+//     mapBlock.setAttribute('class', 'mapBlockShow');
+// });
 
-hidemapButton.addEventListener('click', function() {
-    mapBlock.setAttribute('class', 'mapBlock');
-});
+// hidemapButton.addEventListener('click', function() {
+//     mapBlock.setAttribute('class', 'mapBlock');
+// });
