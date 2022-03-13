6d30523fd42004a23ee85ad11077b4e96a1413a1
Saoa35 <saoamah@gmail.com>
2022-03-13 13:17:06 +0200

    + scale, item width and height

--- Lesson4/3/index.js
+++ Lesson4/3/index.js
@@ -3,9 +3,18 @@ function createDiagram (data, width, height, color) {
     svg.style.width = width;
     svg.style.height = height;
 
-    
+    let maxNumber = Math.max(...data);
+
+    let scale = height / maxNumber;
+    let itemWidth = Math.floor(width / data.length);
+
+    data.map(el => {
+        let item = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+        let itemHeight = el * scale;
+        
+    });
 }
 
 // let ss = [2, 4, 6, 8, 12, 10];
 
-// console.log(Math.max(...ss));
+// console.log(createDiagram(ss, 300, 300));
