a6711177a16cb3421cb0df339788c3924a3cf6b8
Saoa35 <saoamah@gmail.com>
2022-03-13 12:41:14 +0200

    + func

---
+++ Lesson4/3/index.js
@@ -0,0 +1,11 @@
+function createDiagram (data, width, height, color) {
+    let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.style.width = width;
+    svg.style.height = height;
+
+    
+}
+
+// let ss = [2, 4, 6, 8, 12, 10];
+
+// console.log(Math.max(...ss));
