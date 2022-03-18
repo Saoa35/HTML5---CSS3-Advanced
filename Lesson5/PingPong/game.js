a776a4a98a8c45a588c1f86485b968e19f1bde68
Saoa35 <saoamah@gmail.com>
2022-03-18 19:51:18 +0100

    + else in newGame func

--- Lesson5/PingPong/game.js
+++ Lesson5/PingPong/game.js
@@ -2,7 +2,7 @@ window.addEventListener("load", startGame);
 let centerX = 250,
     centerY = 200,
     radius = 15,
-    ballColor = 'red'
+    ballColor = '#368d21'
     dx = 5,
     dy = 5,
     score = 0;
@@ -23,5 +23,41 @@ let centerX = 250,
             document.getElementById("score").innerHTML = "<p>Game over! Your score is:" + score;
             document.getElementById("score").style.border = "2px solid red";
             clearInterval(animation);
+        } else {
+            document.getElementById("span").innerHTML = score;
+            let canvas = document.getElementById("canvas");
+            context = canvas.getContext("2d");
+
+            context.clearRect(0, 0, canvas.width, canvas.height);
+
+            context.beginPath();
+            context.arc(centerX, centerY, radius, 0, Math.PI*2, true);
+            context.stroke();
+            context.fillStyle = ballColor;
+            context.fill();
+
+            if (centerX + radius === gameWidth || centerX - radius ===0) {
+                dx = -dx;
+                score++;
+            }
+
+            if (centerY - radius < 0) {
+                dy = -dy;
+                score++;
+            }
+
+            centerX = centerX + dx;
+            centerY = centerY + dy;
+
+            context.fillStyle = "blue";
+            context.fillRect(racketLeft, racketTop, racketWidth, racketHeight);
+
+            checkCollision();
+            function checkCollision() {
+                if ((centerY + radius == racketTop) && (centerX >= racketLeft) &&(centerX < (racketLeft + racketWidth))) {
+                    dy = -dy;
+                }
+            }
+            
         }
     }
